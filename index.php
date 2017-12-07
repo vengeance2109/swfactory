7cc8caae01d270f0675818e5dff16f7c30e51f88
Unknown <maxime.dedobbeleer@gmail.com>
2017-12-07 10:55:41 +0100

    7/12/2017 10:55

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php
-
-
-echo "lol";
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Software Factory</title>
+    <link rel="stylesheet" href="css.css">
+    <link rel="shortcut icon" href="Foto's/lamp_SxI_icon.ico">
+  </head>
+  <body>
+<ul>
+  <li><a href="https://www.swfactory.be">Home</a></li>
+  <li> <a href="about.php">About us</a> </li>
+</ul>
+<h1>Agency FB</h1>
+<a href="http://www.hoehel.be">hoehel</a>
+  </body>
+</html>
